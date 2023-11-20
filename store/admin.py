4d7e968e1266c283bf619f183aa10a80ab1beeb5
edwin <eduzkipp@gmail.com>
2023-11-20 12:29:27 +0300

    4

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from store.models.customer import Customer
+from store.models.category import Category
+from store.models.orders import Order
+from store.models.product import Products
+
+
+
+
+admin.site.register(Customer)
+admin.site.register(Category)
+admin.site.register(Order)
+admin.site.register(Products)
+
+
+
+
 
 # Register your models here.
--- store/urls.py
+++ store/urls.py
@@ -1,12 +1,15 @@
 from django.contrib import admin
 from django.urls import path
-from .views.home import Index, store
+from .views.home import Index , store
 from .views.signup import Signup
 from .views.login import Login, logout
 from .views.cart import Cart
 from .views.checkout import CheckOut
 from .views.orders import OrderView
 from .middlewares.auth import auth_middleware
+
+
+
 
 
 urlpatterns=[
